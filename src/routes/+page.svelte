c93759a2d768c244db206644ab499de47b11f717
jjones <jimmy.jones@tutanota.com>
2023-10-16 15:54:02 -0600

    add event to capture x and y coordinates of mouse

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,22 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	const width = 960;
+	const height = 500;
+
+	const circleX = width / 2;
+	const circleY = height / 2;
+	const circleRadius = 30;
+
+	const handleMouseMove = (event: MouseEvent) => {
+		const { clientX, clientY } = event;
+		console.log({ clientX, clientY });
+	};
 </script>
+
+<svg {width} {height} on:mousemove={handleMouseMove} role="presentation">
+	<circle cx={circleX} cy={circleY} r={circleRadius} />
+</svg>
 
 {data.data}
