85468bc355dbaef00f2467051e75965b93513024
jjones <jimmy.jones@tutanota.com>
2023-10-17 20:53:41 -0600

    comment out unused code, remove unused import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,26 @@
 <script lang="ts">
 	import { getContext } from 'svelte';
 	import * as d3 from 'd3';
-	import { dataStore } from '$lib/stores/data-store';
 	import ChartStats from '$lib/components/ui/ChartStats.svelte';
 
 	const chartData: any = getContext('csvData');
 
-	const handleAdd = () =>
+	/* const handleAdd = () =>
 		dataStore.update({
 			Specification: 'CSS Level 1',
 			Keyword: 'black',
 			'RGB hex value': '#000000'
-		});
+		}); */
 
 	const width = 960;
 	const height = 500;
 
-	$: console.log('chart data: ', $chartData.csvData);
+	//$: console.log('chart data: ', $chartData.csvData);
 </script>
-
-<div><button on:click={handleAdd}>Hello</button></div>
 
 <ChartStats
 	dataSize={Math.round(d3.csvFormat($chartData.csvData).length / 1024)}
 	numberOfItems={$chartData.csvData.length}
 	numberOfColumns={$chartData.csvData.columns.length}
 />
+<!-- <div><button on:click={handleAdd}>Hello</button></div> -->
